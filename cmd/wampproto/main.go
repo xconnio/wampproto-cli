d36170500c669b101e152f3d2ff67828a1d9d9fd
Omer Akram <omer@thing.com>
2024-06-01 16:57:58 +0500

    integrate wamppproto-go to print hello hex

--- cmd/wampproto/main.go
+++ cmd/wampproto/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"encoding/hex"
+	"log"
+
+	"github.com/xconnio/wampproto-go/messages"
+	"github.com/xconnio/wampproto-go/serializers"
+)
+
 func main() {
-	println("HELLO")
+	hello := messages.NewHello("realm1", "test", nil, nil, []string{"anonymous"})
+	serializer := &serializers.MsgPackSerializer{}
+	data, err := serializer.Serialize(hello)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("HELLO", hex.EncodeToString(data))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/xconnio/wampproto-cli
 
 go 1.20
+
+require github.com/xconnio/wampproto-go v0.0.0-20240531231532-d8fa7f588c4e
+
+require (
+	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+)
