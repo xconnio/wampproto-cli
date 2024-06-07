4f495c2bb100f2fb32e0e7dfed8d46be6f855aee
Muzzammil Shahid <muzzammil@xconn.io>
2024-06-07 15:38:39 +0500

    Add helper functions to convert string lists and maps to typed lists and maps

--- helpers.go
+++ helpers.go
@@ -3,7 +3,9 @@ package wampprotocli
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"strconv"
 )
 
 func HexToBase64(hexStr string) (string, error) {
@@ -28,4 +30,60 @@ func DecodeHexOrBase64(str string) ([]byte, error) {
 	}
 
 	return nil, fmt.Errorf("must be in either hexadecimal or base64 format")
+}
+
+func StringsToTypedList(strings []string) (typedList []any) {
+	for _, value := range strings {
+		if number, errNumber := strconv.Atoi(value); errNumber == nil {
+			typedList = append(typedList, number)
+		} else if float, errFloat := strconv.ParseFloat(value, 64); errFloat == nil {
+			typedList = append(typedList, float)
+		} else if boolean, errBoolean := strconv.ParseBool(value); errBoolean == nil {
+			typedList = append(typedList, boolean)
+		} else {
+			var jsonMap map[string]any
+			var jsonMapList []map[string]any
+			var list []any
+			if errJson := json.Unmarshal([]byte(value), &jsonMap); errJson == nil {
+				typedList = append(typedList, jsonMap)
+			} else if errMapList := json.Unmarshal([]byte(value), &jsonMapList); errMapList == nil {
+				typedList = append(typedList, jsonMapList)
+			} else if errList := json.Unmarshal([]byte(value), &list); errList == nil {
+				typedList = append(typedList, list)
+			} else {
+				typedList = append(typedList, value)
+			}
+		}
+	}
+
+	return typedList
+}
+
+func StringMapToTypedMap(stringMap map[string]string) (typesMap map[string]any) {
+	typesMap = make(map[string]any)
+
+	for key, value := range stringMap {
+		if number, errNumber := strconv.Atoi(value); errNumber == nil {
+			typesMap[key] = number
+		} else if float, errFloat := strconv.ParseFloat(value, 64); errFloat == nil {
+			typesMap[key] = float
+		} else if boolean, errBoolean := strconv.ParseBool(value); errBoolean == nil {
+			typesMap[key] = boolean
+		} else {
+			var jsonMap map[string]any
+			var jsonMapList []map[string]any
+			var list []any
+			if errJson := json.Unmarshal([]byte(value), &jsonMap); errJson == nil {
+				typesMap[key] = jsonMap
+			} else if errMapList := json.Unmarshal([]byte(value), &jsonMapList); errMapList == nil {
+				typesMap[key] = jsonMapList
+			} else if errList := json.Unmarshal([]byte(value), &list); errList == nil {
+				typesMap[key] = list
+			} else {
+				typesMap[key] = value
+			}
+		}
+	}
+
+	return typesMap
 }
--- helpers_test.go
+++ helpers_test.go
@@ -47,3 +47,35 @@ func TestDecodeHexOrBase64(t *testing.T) {
 		require.EqualError(t, err, "must be in either hexadecimal or base64 format")
 	})
 }
+
+func TestStringsToTypedList(t *testing.T) {
+	var input = []string{"false", "789", "45.67", `{"a": true, "b": "c"}`, `[{"x": true}, {"y": false}]`, "world",
+		`["hello", true]`}
+	var expected = []any{false, 789, 45.67, map[string]any{"a": true, "b": "c"}, []map[string]any{{"x": true},
+		{"y": false}}, "world", []any{"hello", true}}
+
+	result := wampprotocli.StringsToTypedList(input)
+	require.Equal(t, expected, result)
+}
+
+func TestStringMapToTypedMap(t *testing.T) {
+	var input = map[string]string{
+		"float":   "45.67",
+		"bool":    "true",
+		"json":    `{"key": "value"}`,
+		"list":    `["a", true, 1.2]`,
+		"string":  "hello",
+		"mapList": `[{"x": true}, {"y": false}]`,
+	}
+	var expected = map[string]any{
+		"float":   45.67,
+		"bool":    true,
+		"json":    map[string]any{"key": "value"},
+		"list":    []any{"a", true, 1.2},
+		"string":  "hello",
+		"mapList": []map[string]any{{"x": true}, {"y": false}},
+	}
+
+	result := wampprotocli.StringMapToTypedMap(input)
+	require.Equal(t, expected, result)
+}
